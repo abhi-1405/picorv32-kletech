da7a072abaa6ee70e2cd4f075b6dd6453129c6ff
rajeshrv <varadharajan.rajesh@gmail.com>
2020-05-21 21:43:07 +0530

    Updated the tests

--- tests/basic_rw.c
+++ tests/basic_rw.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
 int main() { 
+   volatile int *led;
+   volatile int i;
+
+   led = 0x03000000;
+
+   *led = 0x1;
+   for (i=0; i < 10; i++);
+   *led = 0x0;
+   for (i=0; i < 10; i++);
+   *led = 0x3;
+   for (i=0; i < 10; i++);
+   *led = 0x0;
+   for (i=0; i < 10; i++);
+   *led = 0x7;
+   for (i=0; i < 10; i++);
+   *led = 0x0;
+   for (i=0; i < 10; i++);
+   *led = 0xf;
+   for (i=0; i < 10; i++);
+   *led = 0x0;
+
    return 0;
 }
